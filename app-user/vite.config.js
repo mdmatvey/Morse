feba61fc08bf1a03927b5d96294d558ae736ba23
mdmatvey <lyoshinmdmatvey@gmail.com>
2025-03-23 23:48:21 +0300

    core: created admin-panel frontend app

---
+++ app-user/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+    root: path.resolve(__dirname, 'src'),
+    server: {
+        port: 5173,
+        open: true,
+    },
+    build: {
+        outDir: '../../build/user-static',
+        emptyOutDir: true,
+    },
+});
