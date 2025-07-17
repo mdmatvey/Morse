fe5e8a6c6e370897dfe7c67e06d86363a07f5180
mdmatvey <104563053+mdmatvey@users.noreply.github.com>
2025-07-17 13:28:05 +0300

    feat: added manual mode

--- app-user/src/core/morse/MorseAudioPlayer.js
+++ app-user/src/core/morse/MorseAudioPlayer.js
@@ -3,6 +3,7 @@ import { SPEED_CONFIG } from './constants.js';
 export class MorseAudioPlayer {
     constructor() {
         this._audioContext = null;
+        this._oscillator = null;
     }
 
     playSequence(sequence, baseDuration, tone, letterPause, groupPause) {
@@ -12,34 +13,46 @@ export class MorseAudioPlayer {
             '-': baseDuration * SPEED_CONFIG.DASH_MULTIPLIER,
         };
 
-        sequence.split('').forEach((symbol, index, array) => {
-            const symbolDuration = durations[symbol] || 0;
-
-            if (symbolDuration > 0) {
-                setTimeout(() => this._playSignal(symbolDuration, tone), delay);
-                delay += symbolDuration + baseDuration;
+        sequence.split('').forEach((symbol, idx, arr) => {
+            const dur = durations[symbol] || 0;
+            if (dur > 0) {
+                setTimeout(() => this._playSignal(dur, tone), delay);
+                delay += dur + baseDuration;
             }
-
             if (symbol === ' ') {
-                if (array[index + 1] === ' ') {
-                    delay += groupPause;
-                } else {
-                    delay += letterPause;
-                }
+                delay += arr[idx + 1] === ' ' ? groupPause : letterPause;
             }
         });
     }
 
+    // Запускаем непрерывный тон
+    startContinuous(baseDuration, tone) {
+        if (this._oscillator) return;
+        const ctx = this._getAudioContext();
+        const osc = ctx.createOscillator();
+        osc.type = 'sine';
+        osc.frequency.setValueAtTime(tone, ctx.currentTime);
+        osc.connect(ctx.destination);
+        osc.start();
+        this._oscillator = osc;
+    }
+
+    // Останавливаем непрерывный тон
+    stopContinuous() {
+        if (!this._oscillator) return;
+        this._oscillator.stop();
+        this._oscillator.disconnect();
+        this._oscillator = null;
+    }
+
     _playSignal(duration, tone) {
-        const context = this._getAudioContext();
-        const oscillator = context.createOscillator();
-
-        oscillator.type = 'sine';
-        oscillator.frequency.setValueAtTime(tone, context.currentTime);
-        oscillator.connect(context.destination);
-        oscillator.start();
-
-        setTimeout(() => oscillator.stop(), duration);
+        const ctx = this._getAudioContext();
+        const osc = ctx.createOscillator();
+        osc.type = 'sine';
+        osc.frequency.setValueAtTime(tone, ctx.currentTime);
+        osc.connect(ctx.destination);
+        osc.start();
+        setTimeout(() => osc.stop(), duration);
     }
 
     _getAudioContext() {
@@ -47,14 +60,6 @@ export class MorseAudioPlayer {
             this._audioContext = new (window.AudioContext ||
                 window.webkitAudioContext)();
         }
-
         return this._audioContext;
-    }
-
-    _cleanupAudioContext() {
-        if (this._audioContext) {
-            this._audioContext.close();
-            this._audioContext = null;
-        }
     }
 }
