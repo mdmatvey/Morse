8d06aeb4d83c78f3cfddcdbff11c349d3a7bcfe4
mdmatvey <104563053+mdmatvey@users.noreply.github.com>
2025-07-19 13:45:27 +0300

    feat: added clients busy status logic

--- server/controllers/signalController.js
+++ server/controllers/signalController.js
@@ -2,6 +2,8 @@ import {
     registerClient,
     unregisterClient,
     sendMessage,
+    setBusy,
+    setFree,
 } from '../services/websocketService.js';
 
 export function handleConnection(ws, req) {
@@ -19,6 +21,12 @@ export function handleConnection(ws, req) {
                 break;
             case 'message':
                 sendMessage(data.recipient, data.content, data.params);
+                break;
+            case 'set-busy':
+                setBusy(data.userId, data.partnerId);
+                break;
+            case 'set-free':
+                setFree(data.userId);
                 break;
         }
     });
